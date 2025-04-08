8a9c1b9d505a88a9188509d4d26f87405c3f3068
Julian Rosalen <julian.rosalen@miracledevs.com>
2025-04-08 17:33:33 -0300

    press detection

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./App.css";
 import useGamepadController from "./hooks/useGamepad";
 
+const FACE_1_INDEX = 0; // Índice de FACE_1 (X en PS, A en Xbox)
+
 const App: React.FC = () => {
   const gamepads = useGamepadController();
+  const [buttonPressed, setButtonPressed] = useState(false);
+
+  //Button press detection for reaction
+  useEffect(() => {
+    Object.entries(gamepads).forEach(([index, gamepad]) => {
+      if (gamepad.buttons[FACE_1_INDEX] && !buttonPressed) {
+        console.log(`FACE_1 pressed with index - ${index}`);
+        setButtonPressed(true);
+      } else if (!gamepad.buttons[FACE_1_INDEX] && buttonPressed) {
+        setButtonPressed(false);
+      }
+    });
+  }, [gamepads, buttonPressed]);
 
   return (
     <div>
